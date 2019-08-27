7b096d87ba08debe77e650b6de82e43105d1d304
HankSeametrics <hbeck@seametrics.com>
2019-08-27 12:14:12 -0700

    Followed a tutorial on Tkinter

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MVCpractice
 Practice with the MVC framework to better understand CSapp
+
+End Goal is to have a GUI that can be used to set up a math equation, execute it, and display the results
--- myView.py
+++ myView.py
@@ -1,2 +1,33 @@
 import Tkinter
 from Tkinter import *
+
+class Application(Frame):
+    def say_hi(self):
+        print "Howdy Howdy Howdy"
+
+    def createWidgets(self):
+        self.QUIT =Button(self)
+        self.QUIT["text"] = "STOP"
+        self.QUIT["fg"] = "red"
+        self.QUIT["command"] = self.quit
+
+        self.QUIT.pack({"side": "left"})
+
+        self.hi_there = Button(self)
+        self.hi_there["text"] = "hey Partner"
+        self.hi_there["command"] = self.say_hi
+
+        self.hi_there.pack({"side":"left"})
+    
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+try:
+    root =Tk()
+    app =Application(master=root)
+    app.mainloop()
+    root.destroy()
+except Exception as ex:
+    print ex
+    raw_input()
